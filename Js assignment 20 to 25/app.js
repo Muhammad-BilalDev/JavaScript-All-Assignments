aa60be2722520779e08cf0913f81e9f3f8d4eec0
Muhammad-BilalDev <muhammadbilal70150@gmail.com>
2026-04-24 02:27:42 +0500

    Half Done

---
+++ Js assignment 20 to 25/app.js
@@ -0,0 +1,96 @@
+// Assignment 20 to 25 
+// ans (1)
+// let firstName = prompt("Enter your first name")
+// let lasttName = prompt("Enter your last name")
+
+// let addName = [firstName , lasttName].join(" ")
+// document.write(addName)
+
+
+// ans(2)
+// let userInput = prompt("Enter your favorite mobile")
+
+// let userInputLength = userInput.length
+
+// document.write("My favorite phone is: ",userInput,"<br>")
+// document.write("lenght of ",typeof(userInput)," ",userInputLength)
+
+// ans(3)
+// let nam = "pakistan"
+// let checkIndex = nam.indexOf("n")
+
+// document.write(typeof(nam)+": "+nam+"<br>")
+// document.write("Index of 'n': ",checkIndex)
+
+// ans(4)
+
+// let nam = "Hello World"
+// let checkLastIndex = nam.lastIndexOf("l")
+
+// document.write(typeof(nam)+": "+nam+"<br>")
+// document.write("Index of 'l': ",checkLastIndex)
+
+// ans(5)
+// let nam = "pakistan"
+// let checkIndex = nam.charAt(3)
+
+// document.write("String: " + nam + "<br>");
+// document.write("Character at index 3: " + checkIndex);
+
+
+// ans(6)
+// let firstName = prompt("Enter your first name")
+// let lastName = prompt("Enter your last name")
+
+// let addName = firstName.concat(" ",lastName)
+// document.write(addName)
+
+
+// ans(7)
+// let nam = "Hyderabad"
+// let namRplact = nam.replace("Hyderabad","Islamabad")
+
+// document.write("City: ",nam,"<br>")
+// document.write("After replacement: ",namRplact)
+
+
+// ans(8)
+// var message = "Ali and Sami are best friends. They play cricket and football to gether.";
+// let updatedsMessage = message.replaceAll("and","&")
+// document.write(updatedsMessage)
+
+
+// ans(9)
+
+// let number = "472"
+
+// let numberChange = Number(number)
+// document.writeln("Value: ",numberChange,"<br>")
+// document.writeln("Type: ",typeof(number),"<br>")
+// document.writeln("Value: ",numberChange,"<br>")
+// document.writeln("Type: ",typeof(numberChange))
+
+
+// ans(10)
+// let text = prompt("Enter yor text")
+// let capitalTaxt = text.toUpperCase()
+// document.write("User input: ",text,"<br>")
+// document.write("Upper case: ",capitalTaxt)
+
+
+// ans(11)
+// let text = prompt("Enter yor text")
+// let capitalTaxt1 = text.slice(0,1).toUpperCase()
+// let capitalTaxt2 = text.slice(1).toLowerCase()
+// let concatText = capitalTaxt1 + capitalTaxt2
+// document.write("User input: ",text,"<br>")
+// document.write("Title case: ",concatText)
+
+
+// ans(12)
+// let num = 35.36;
+// let updateNumber = String(num).replace(".","")
+// document.write("Number: ",num,"<br>")
+// document.write("Result: ",updateNumber)
+
+
